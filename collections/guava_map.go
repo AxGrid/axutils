2b473a7b71083e6e38a56d1693f6040357119c30
Dmitry <dmitry.vysochin@gmail.com>
2024-03-15 12:39:15 +0100

    guava

--- collections/guava_map.go
+++ collections/guava_map.go
@@ -1,7 +1,9 @@
 package collections
 
 import (
+	"context"
 	"sync"
+	"time"
 )
 
 /*
@@ -13,17 +15,55 @@ zed (14.03.2024)
 */
 
 type GuavaLoadFunc[K comparable, V any] func(K) (V, error)
+type GuavaUnloadFunc[K comparable, V any] func(K, V)
+
+type timeoutHolder[K comparable] struct {
+	key  K
+	time time.Time
+}
+
 type GuavaMap[K comparable, V any] struct {
-	stored      map[K]V
-	storedSlice []K
-	mu          sync.RWMutex
-	maxCount    int
-	loadFunc    GuavaLoadFunc[K, V]
+	stored             map[K]V
+	storedSlice        []K
+	mu                 sync.RWMutex
+	maxCount           int
+	loadFunc           GuavaLoadFunc[K, V]
+	unloadFunc         GuavaUnloadFunc[K, V]
+	enableWriteTimeout bool
+	writeTimeout       time.Duration
+	timeoutSlices      []*timeoutHolder[K]
+	timeoutMu          sync.Mutex
+	clearTimeout       time.Duration
+	ctx                context.Context
+}
+
+func (m *GuavaMap[K, V]) timeoutThreadFunction() {
+	for {
+		select {
+		case <-m.ctx.Done():
+			return
+		case <-time.After(m.clearTimeout):
+			m.timeoutMu.Lock()
+			deleteList := make([]K, 0)
+			for len(m.timeoutSlices) > 0 && time.Since(m.timeoutSlices[0].time) >= m.writeTimeout {
+				key := m.timeoutSlices[0].key
+				m.timeoutSlices = m.timeoutSlices[1:]
+				deleteList = append(deleteList, key)
+			}
+			m.timeoutMu.Unlock()
+			for _, key := range deleteList {
+				m.Delete(key)
+			}
+		}
+	}
 }
 
 func (m *GuavaMap[K, V]) Delete(key K) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
+	if m.unloadFunc != nil {
+		go m.unloadFunc(key, m.stored[key])
+	}
 	delete(m.stored, key)
 	if m.maxCount > 0 {
 		for i, k := range m.storedSlice {
@@ -33,13 +73,36 @@ func (m *GuavaMap[K, V]) Delete(key K) {
 			}
 		}
 	}
+	if m.enableWriteTimeout {
+		m.timeoutMu.Lock()
+		for i, k := range m.timeoutSlices {
+			if k.key == key {
+				m.timeoutSlices = append(m.timeoutSlices[:i], m.timeoutSlices[i+1:]...)
+				break
+			}
+		}
+		m.timeoutMu.Unlock()
+	}
 }
 
 func (m *GuavaMap[K, V]) Clear() {
 	m.mu.Lock()
 	defer m.mu.Unlock()
+	if m.unloadFunc != nil {
+		for k, v := range m.stored {
+			go m.unloadFunc(k, v)
+		}
+	}
 	m.stored = make(map[K]V)
-	m.storedSlice = make([]K, 0, m.maxCount)
+	if m.maxCount > 0 {
+		m.storedSlice = make([]K, 0, m.maxCount)
+	}
+	if m.enableWriteTimeout {
+		m.timeoutMu.Lock()
+		defer m.timeoutMu.Unlock()
+		m.timeoutSlices = make([]*timeoutHolder[K], 0)
+	}
+
 }
 
 func (m *GuavaMap[K, V]) Size() int {
@@ -70,10 +133,23 @@ func (m *GuavaMap[K, V]) Get(key K) (V, error) {
 	}
 	if m.maxCount > 0 {
 		if len(m.storedSlice) >= m.maxCount {
+			if m.unloadFunc != nil {
+				go m.unloadFunc(m.storedSlice[0], m.stored[m.storedSlice[0]])
+			}
 			delete(m.stored, m.storedSlice[0])
 			m.storedSlice = m.storedSlice[1:]
+			if m.enableWriteTimeout {
+				m.timeoutMu.Lock()
+				m.timeoutSlices = m.timeoutSlices[1:]
+				m.timeoutMu.Unlock()
+			}
 		}
 		m.storedSlice = append(m.storedSlice, key)
+	}
+	if m.enableWriteTimeout {
+		m.timeoutMu.Lock()
+		m.timeoutSlices = append(m.timeoutSlices, &timeoutHolder[K]{key: key, time: time.Now()})
+		m.timeoutMu.Unlock()
 	}
 	m.stored[key] = loadedVal
 	return loadedVal, nil
@@ -89,20 +165,51 @@ func (m *GuavaMap[K, V]) Set(key K, val V) {
 	}
 	if m.maxCount > 0 {
 		if len(m.storedSlice) >= m.maxCount {
+			if m.unloadFunc != nil {
+				go m.unloadFunc(m.storedSlice[0], m.stored[m.storedSlice[0]])
+			}
 			delete(m.stored, m.storedSlice[0])
 			m.storedSlice = m.storedSlice[1:]
+			if m.enableWriteTimeout {
+				m.timeoutMu.Lock()
+				m.timeoutSlices = m.timeoutSlices[1:]
+				m.timeoutMu.Unlock()
+			}
 		}
 		m.storedSlice = append(m.storedSlice, key)
+	}
+	if m.enableWriteTimeout {
+		m.timeoutMu.Lock()
+		m.timeoutSlices = append(m.timeoutSlices, &timeoutHolder[K]{key: key, time: time.Now()})
+		m.timeoutMu.Unlock()
 	}
 }
 
 type GuavaMapBuilder[K comparable, V any] struct {
-	loadFunc GuavaLoadFunc[K, V]
-	maxCount int
+	loadFunc           GuavaLoadFunc[K, V]
+	maxCount           int
+	unloadFunc         GuavaUnloadFunc[K, V]
+	writeTimeout       time.Duration
+	enableWriteTimeout bool
+	ctx                context.Context
+	clearTimeout       time.Duration
 }
 
 func NewGuavaMap[K comparable, V any]() *GuavaMapBuilder[K, V] {
-	return &GuavaMapBuilder[K, V]{}
+	return &GuavaMapBuilder[K, V]{
+		ctx:          context.Background(),
+		clearTimeout: time.Second * 5,
+	}
+}
+
+func (b *GuavaMapBuilder[K, V]) WithContext(ctx context.Context) *GuavaMapBuilder[K, V] {
+	b.ctx = ctx
+	return b
+}
+
+func (b *GuavaMapBuilder[K, V]) WithClearTimeout(clearTimeout time.Duration) *GuavaMapBuilder[K, V] {
+	b.clearTimeout = clearTimeout
+	return b
 }
 
 func (b *GuavaMapBuilder[K, V]) WithLoadFunc(loadFunc GuavaLoadFunc[K, V]) *GuavaMapBuilder[K, V] {
@@ -115,14 +222,32 @@ func (b *GuavaMapBuilder[K, V]) WithMaxCount(maxCount int) *GuavaMapBuilder[K, V
 	return b
 }
 
+func (b *GuavaMapBuilder[K, V]) WithUnloadFunc(unloadFunc GuavaUnloadFunc[K, V]) *GuavaMapBuilder[K, V] {
+	b.unloadFunc = unloadFunc
+	return b
+}
+
+func (b *GuavaMapBuilder[K, V]) WithWriteTimeout(writeTimeout time.Duration) *GuavaMapBuilder[K, V] {
+	b.writeTimeout = writeTimeout
+	b.enableWriteTimeout = b.writeTimeout > 0
+	return b
+}
+
 func (b *GuavaMapBuilder[K, V]) Build() *GuavaMap[K, V] {
 	res := &GuavaMap[K, V]{
-		stored:   make(map[K]V),
-		loadFunc: b.loadFunc,
-		maxCount: b.maxCount,
+		stored:             make(map[K]V),
+		loadFunc:           b.loadFunc,
+		maxCount:           b.maxCount,
+		unloadFunc:         b.unloadFunc,
+		writeTimeout:       b.writeTimeout,
+		enableWriteTimeout: b.enableWriteTimeout,
+		ctx:                b.ctx,
 	}
 	if b.maxCount > 0 {
 		res.storedSlice = make([]K, 0, b.maxCount)
+	}
+	if b.enableWriteTimeout {
+		go res.timeoutThreadFunction()
 	}
 	return res
 }
--- collections/guava_map_test.go
+++ collections/guava_map_test.go
@@ -3,6 +3,7 @@ package collections
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 /*
@@ -17,7 +18,6 @@ func TestGuavaMap_Get(t *testing.T) {
 	buildCount := 0
 	m := NewGuavaMap[int, int]().WithMaxCount(10).WithLoadFunc(func(key int) (int, error) {
 		buildCount++
-		t.Log("build", key)
 		return key * 10, nil
 	}).Build()
 	v, err := m.Get(10)
@@ -35,4 +35,100 @@ func TestGuavaMap_Get(t *testing.T) {
 	v, err = m.Get(15)
 	assert.Nil(t, err)
 	assert.Equal(t, 500, v)
+}
+
+func TestGuavaMap_HiLoad(t *testing.T) {
+	buildCount := 0
+	m := NewGuavaMap[int, int]().WithMaxCount(10).WithLoadFunc(func(key int) (int, error) {
+		time.Sleep(100 * time.Millisecond)
+		buildCount++
+		return key * 10, nil
+	}).Build()
+	go func() {
+		v, err := m.Get(10)
+		assert.Nil(t, err)
+		assert.Equal(t, 100, v)
+	}()
+
+	go func() {
+		v, err := m.Get(15)
+		assert.Nil(t, err)
+		assert.Equal(t, 150, v)
+	}()
+
+	go func() {
+		v, err := m.Get(10)
+		assert.Nil(t, err)
+		assert.Equal(t, 100, v)
+	}()
+	time.Sleep(250 * time.Millisecond)
+	assert.Equal(t, 3, buildCount)
+	v, err := m.Get(10)
+	assert.Nil(t, err)
+	assert.Equal(t, 100, v)
+	assert.Equal(t, 3, buildCount)
+}
+
+func TestGuavaMap_GetWithMaxCount(t *testing.T) {
+	buildCount := 0
+	unloadCount := 0
+	m := NewGuavaMap[int, int]().WithMaxCount(10).WithLoadFunc(func(key int) (int, error) {
+		buildCount++
+		return key * 10, nil
+	}).WithUnloadFunc(func(key int, value int) {
+		unloadCount++
+	}).
+		WithMaxCount(50).Build()
+	for i := 0; i < 100; i++ {
+		v, err := m.Get(i)
+		assert.Nil(t, err)
+		assert.Equal(t, i*10, v)
+	}
+	assert.Equal(t, 50, m.Size())
+	assert.Equal(t, 100, buildCount)
+	time.Sleep(time.Millisecond * 100)
+	assert.Equal(t, 50, unloadCount)
+}
+
+func TestGuavaMap_GetWithWriteTimeout(t *testing.T) {
+	buildCount := 0
+	unloadCount := 0
+	m := NewGuavaMap[int, int]().WithMaxCount(10).WithLoadFunc(func(key int) (int, error) {
+		buildCount++
+		return key * 10, nil
+	}).WithUnloadFunc(func(key int, value int) {
+		unloadCount++
+	}).
+		WithMaxCount(50).WithWriteTimeout(time.Millisecond * 20).WithClearTimeout(time.Millisecond * 10).Build()
+	for i := 0; i < 100; i++ {
+		v, err := m.Get(i)
+		assert.Nil(t, err)
+		assert.Equal(t, i*10, v)
+	}
+	assert.Equal(t, 50, m.Size())
+	assert.Equal(t, 100, buildCount)
+	time.Sleep(time.Millisecond * 1000)
+	assert.Equal(t, 100, unloadCount)
+	assert.Equal(t, 0, m.Size())
+
+}
+
+func BenchmarkGuavaMap_Get(b *testing.B) {
+	m := NewGuavaMap[int, int]().WithMaxCount(10).WithLoadFunc(func(key int) (int, error) {
+		return key * 10, nil
+	}).Build()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m.Get(i)
+	}
+}
+
+func BenchmarkGuavaMap_GetWithMaxCount(b *testing.B) {
+	m := NewGuavaMap[int, int]().WithMaxCount(10).WithLoadFunc(func(key int) (int, error) {
+		return key * 10, nil
+	}).WithMaxCount(50).Build()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		m.Get(i)
+	}
 }
