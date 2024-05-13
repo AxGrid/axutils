272c712bf033fc3e9536c1d1502135f39eb8a4df
Илья Каран <hucci.royce@gmail.com>
2024-05-13 16:12:23 +0300

    multiple responses map, test

--- collections/response_map.go
+++ collections/response_map.go
@@ -13,57 +13,97 @@ import (
 zed (11.04.2024)
 */
 
-type ResponseMap[K comparable, V any] struct {
+type ResponseMapBuilder[K comparable, V any] struct {
 	timeout int
-	mu      sync.RWMutex
-	m       map[K]chan V
 }
 
-func NewResponseMap[K comparable, V any](timeout int) *ResponseMap[K, V] {
-	return &ResponseMap[K, V]{
-		timeout: timeout,
-		mu:      sync.RWMutex{},
-		m:       make(map[K]chan V),
+func NewResponseMap[K comparable, V any]() *ResponseMapBuilder[K, V] {
+	return &ResponseMapBuilder[K, V]{
+		timeout: 300,
 	}
 }
 
-func (r *ResponseMap[K, V]) getChan(key K) chan V {
+func (b *ResponseMapBuilder[K, V]) WithTimeout(timeout int) *ResponseMapBuilder[K, V] {
+	b.timeout = timeout
+	return b
+}
+
+func (b *ResponseMapBuilder[K, V]) Build() *ResponseMap[K, V] {
+	rm := &ResponseMap[K, V]{
+		timeout: b.timeout,
+		mu:      sync.RWMutex{},
+		m:       make(map[K]*chansHolder[V]),
+	}
+	return rm
+}
+
+type ResponseMap[K comparable, V any] struct {
+	timeout int
+	mu      sync.RWMutex
+	m       map[K]*chansHolder[V]
+}
+
+type chansHolder[V any] struct {
+	t     *time.Timer
+	chans []chan V
+}
+
+func (r *ResponseMap[K, V]) Set(key K, value V) {
 	r.mu.RLock()
-	ch, ok := r.m[key]
+	holder, ok := r.m[key]
+	r.mu.RUnlock()
+	if !ok {
+		return
+	}
+	for _, ch := range holder.chans {
+		ch <- value
+	}
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	delete(r.m, key)
+}
+
+func (r *ResponseMap[K, V]) Wait(key K) V {
+	return <-r.getChan(key)
+}
+
+func (r *ResponseMap[K, V]) getChan(key K) chan V {
+	ch := make(chan V)
+	r.mu.RLock()
+	holder, ok := r.m[key]
 	r.mu.RUnlock()
 	if ok {
+		r.mu.Lock()
+		defer r.mu.Unlock()
+		holder.chans = append(r.m[key].chans, ch)
+		r.m[key] = holder
 		return ch
 	}
 	r.mu.Lock()
 	defer r.mu.Unlock()
-	ch, ok = r.m[key]
+	holder, ok = r.m[key]
 	if ok {
+		holder.chans = append(r.m[key].chans, ch)
+		r.m[key] = holder
 		return ch
 	}
-	ch = make(chan V, 1)
-	r.m[key] = ch
-	return ch
-}
-
-func (r *ResponseMap[K, V]) Set(key K, value V) {
-	r.getChan(key) <- value
-}
-
-func (r *ResponseMap[K, V]) Wait(key K) V {
-	t := time.NewTimer(time.Duration(r.timeout) * time.Second)
+	holder = &chansHolder[V]{
+		t:     time.NewTimer(time.Duration(r.timeout) * time.Second),
+		chans: make([]chan V, 0),
+	}
+	holder.chans = append(holder.chans, ch)
+	r.m[key] = holder
 	go func() {
-		for {
-			select {
-			case <-t.C:
-				r.mu.RLock()
-				close(r.m[key])
-				r.mu.RUnlock()
+		defer holder.t.Stop()
+		select {
+		case <-holder.t.C:
+			for _, ch := range holder.chans {
+				close(ch)
 			}
+			r.mu.Lock()
+			defer r.mu.Unlock()
+			delete(r.m, key)
 		}
 	}()
-	res := <-r.getChan(key)
-	r.mu.Lock()
-	delete(r.m, key)
-	r.mu.Unlock()
-	return res
+	return ch
 }
--- collections/response_map_test.go
+++ collections/response_map_test.go
@@ -11,8 +11,33 @@ type mockResponse struct {
 	err  error
 }
 
+//func TestResponseMap_Wait(t *testing.T) {
+//	m := NewResponseMap[uint64, *mockResponse](1)
+//
+//	go func() {
+//		time.Sleep(2 * time.Second)
+//		m.Set(1, &mockResponse{data: []byte("1")})
+//	}()
+//
+//	res := m.Wait(1)
+//	assert.Equal(t, []byte("1"), res.data)
+//	assert.Nil(t, res.err)
+//}
+//
+//func TestResponseMap_Wait_Timeout(t *testing.T) {
+//	m := NewResponseMap[uint64, *mockResponse](1)
+//
+//	go func() {
+//		time.Sleep(3 * time.Second)
+//		m.Set(1, &mockResponse{data: []byte("1")})
+//	}()
+//
+//	res := m.Wait(1)
+//	assert.Nil(t, res)
+//}
+
 func TestResponseMap_Wait(t *testing.T) {
-	m := NewResponseMap[uint64, *mockResponse](1)
+	m := NewResponseMap[uint64, *mockResponse]().Build()
 
 	go func() {
 		time.Sleep(2 * time.Second)
@@ -25,7 +50,7 @@ func TestResponseMap_Wait(t *testing.T) {
 }
 
 func TestResponseMap_Wait_Timeout(t *testing.T) {
-	m := NewResponseMap[uint64, *mockResponse](1)
+	m := NewResponseMap[uint64, *mockResponse]().WithTimeout(1).Build()
 
 	go func() {
 		time.Sleep(3 * time.Second)
@@ -34,4 +59,50 @@ func TestResponseMap_Wait_Timeout(t *testing.T) {
 
 	res := m.Wait(1)
 	assert.Nil(t, res)
+}
+
+func TestResponseMap_Wait_Multiple_Keys(t *testing.T) {
+	m := NewResponseMap[uint64, *mockResponse]().Build()
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		assert.Equal(t, 100, len(m.m[1].chans))
+	}()
+
+	go func() {
+		time.Sleep(3 * time.Second)
+		m.Set(1, &mockResponse{data: []byte("1")})
+	}()
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			res := m.Wait(1)
+			assert.NotNil(t, res)
+			assert.Equal(t, []byte("1"), res.data)
+			assert.Nil(t, res.err)
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+}
+
+func TestResponseMap_Wait_Multiple_Keys_Timeout(t *testing.T) {
+	m := NewResponseMap[uint64, *mockResponse]().WithTimeout(1).Build()
+
+	go func() {
+		time.Sleep(3 * time.Second)
+		m.Set(1, &mockResponse{data: []byte("1")})
+	}()
+
+	shouldResps := 100
+	resps := 0
+	for i := 0; i < shouldResps; i++ {
+		go func() {
+			res := m.Wait(1)
+			assert.Nil(t, res)
+			resps++
+		}()
+	}
+	time.Sleep(4 * time.Second)
+	assert.Equal(t, shouldResps, resps)
 }
